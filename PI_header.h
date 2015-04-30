18b2f36380f55f636f23190c25d4af651546663c
John Tramm <john.tramm@gmail.com>
2015-04-30 10:43:33 -0400

    added materials init function

--- PI_header.h
+++ PI_header.h
@@ -2,3 +2,12 @@
 #include<stdlib.h>
 #include<math.h>
 
+typedef struct{
+	double D1;
+	double D2;
+	double Sigma_A1;
+	double Sigma_A2;
+	double Sigma_S;
+	double Sigma_F1;
+	double Sigma_F2;
+} Material;
